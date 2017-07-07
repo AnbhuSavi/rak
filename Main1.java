1c8803acdf18bd15f0a5e73661b3d0ff20d50529
AnbhuSavi <saviprabhu123@gmail.com>
2017-07-07 09:18:41 +0530

    Create Main1.java

---
+++ Main1.java
@@ -0,0 +1,18 @@
+import java.*;
+import java.util.*;
+public class Main1
+{
+public static void main(String args[])
+{
+  
+ 
+  Scanner ar=new Scanner(System.in);
+   String s = "Hello";
+   for(int i=0;i<5;i++)
+   {
+     
+  System.out.println(s);
+  
+   }
+   }
+   }
